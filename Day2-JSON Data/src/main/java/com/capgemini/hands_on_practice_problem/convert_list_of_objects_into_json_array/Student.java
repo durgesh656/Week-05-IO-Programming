c0ee64ecbcd16a648cef35cfec0875309ddca741
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-21 00:32:43 +0530

    [Durgesh kushwaha] Add - added the solution of 'convert list of objects into json array' problem✨

---
+++ Day2-JSON Data/src/main/java/com/capgemini/hands_on_practice_problem/convert_list_of_objects_into_json_array/Student.java
@@ -0,0 +1,40 @@
+package com.capgemini.hands_on_practice_problem.convert_list_of_objects_into_json_array;
+
+public class Student {
+    private String name;
+    private int age;
+    private String email;
+
+    // Constructors
+    public Student() {}  // Default constructor
+
+    public Student(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
